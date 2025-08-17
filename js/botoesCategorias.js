7fbfac8c72f0f82209f2f9d4829bc8523997de04
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-08-16 22:27:11 -0300

    refactor(botões-categoria) implemetar atualização dinamica do atributo ariaSelected, ajustando mudança da classe ao atributo

--- js/botoesCategorias.js
+++ js/botoesCategorias.js
@@ -13,19 +13,22 @@ function filtrarPorCategoria(filtro) {
   const videos = document.querySelectorAll(".videos__item");
 
   for (const video of videos) {
-    const categoria = video.querySelector(".categoria").textContent.toLowerCase();
+    const categoria = video
+      .querySelector(".categoria")
+      .textContent.toLowerCase();
     const valorFiltro = filtro.toLowerCase();
 
-    const mostrarVideo = valorFiltro === 'tudo' || categoria.includes(valorFiltro);
+    const mostrarVideo =
+      valorFiltro === "tudo" || categoria.includes(valorFiltro);
 
     video.classList.toggle("escondido", !mostrarVideo);
   }
 }
-
 function atualizarEstadosDosBotoes(categoriaSelecionada) {
   botoesCategorias.forEach((botao) => {
-    const botaoFoiSelecionado = botao.getAttribute("name") === categoriaSelecionada;
+    const botaoFoiSelecionado =
+      botao.getAttribute("name") === categoriaSelecionada;
 
-    botao.classList.toggle("selecionado", botaoFoiSelecionado);
-  })
+    botao.ariaSelected = botaoFoiSelecionado;
+  });
 }
